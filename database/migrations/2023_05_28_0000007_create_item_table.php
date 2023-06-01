c093978cfd6fe180eb2348546065ccc967e4792a
bungasav <bungapramestis86@gmail.com>
2023-06-01 23:48:31 +0700

    fix nullable for supplier

--- database/migrations/2023_05_28_0000007_create_item_table.php
+++ database/migrations/2023_05_28_0000007_create_item_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('item', function (Blueprint $table) {
             $table->id('ItemId');
             $table->string('Name');
-            $table->bigInteger('SupplierId',false,true);
+            $table->bigInteger('SupplierId');
             $table->bigInteger('UserId',false,true);
             $table->string('Description',500);
             $table->integer('UnitInStock');
